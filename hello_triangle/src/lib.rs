64202e982a6fd22de6a3a8409a3102deafc51d6a
Lord-Lelsers <48894200+LelsersLasers@users.noreply.github.com>
2022-07-26 18:05:41 -0700

    part 2: WASM window works

--- hello_triangle/src/lib.rs
+++ hello_triangle/src/lib.rs
@@ -30,7 +30,7 @@ pub fn run() {
         web_sys::window()
             .and_then(|win| win.document())
             .and_then(|doc| {
-                let dst = doc.get_element_by_id("wasm-example")?;
+                let dst = doc.get_element_by_id("body")?;
                 let canvas = web_sys::Element::from(window.canvas());
                 dst.append_child(&canvas).ok()?;
                 Some(())
